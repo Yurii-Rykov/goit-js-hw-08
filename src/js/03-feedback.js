a34651c6a8b2fe8536b32081329c968993352d5d
Yurii-Rykov <103988434+Yurii-Rykov@users.noreply.github.com>
2022-07-21 11:31:24 +0300

    corect

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,39 +4,39 @@ const formEl = document.querySelector('.feedback-form');
 const textEl = document.querySelector('.feedback-form textarea');
 const emailEl = document.querySelector('.feedback-form input');
 const FEEDBACK = 'feedback-form-state';
-let obj = {}
+let obj = {};
 
 formEl.addEventListener('submit', onSubmitForm);
 formEl.addEventListener('input', throttle(textInput, 500));
 
-textAreaValue()
+textAreaValue();
 
 function onSubmitForm(evt) {
-    evt.preventDefault();
-    if (emailEl.value === '' || textEl.value === '') {
-        alert('Please fill in all the fields!')
-        return
-    }
-    console.log(obj);
-    let obj = {}
-    evt.currentTarget.reset();
-    localStorage.removeItem(FEEDBACK);
+  evt.preventDefault();
+  if (emailEl.value === '' || textEl.value === '') {
+    alert('Please fill in all the fields!');
+    return;
+  }
+  let obj = {};
+  evt.currentTarget.reset();
+  localStorage.removeItem(FEEDBACK);
+  console.log(obj);
 }
 
 function textInput(evt) {
-    obj[evt.target.name] = evt.target.value
-    localStorage.setItem(FEEDBACK, JSON.stringify(obj)); 
+  obj[evt.target.name] = evt.target.value;
+  localStorage.setItem(FEEDBACK, JSON.stringify(obj));
 }
 
 function textAreaValue() {
-    const saveMessage = localStorage.getItem(FEEDBACK);
-    if (saveMessage) {
-        obj = JSON.parse(saveMessage);
-        if (obj.email) {
-            emailEl.value = obj.email
-        }
-        if (obj.message) {
-            textEl.value = obj.message
-        }
+  const saveMessage = localStorage.getItem(FEEDBACK);
+  if (saveMessage) {
+    obj = JSON.parse(saveMessage);
+    if (obj.email) {
+      emailEl.value = obj.email;
     }
+    if (obj.message) {
+      textEl.value = obj.message;
+    }
+  }
 }
